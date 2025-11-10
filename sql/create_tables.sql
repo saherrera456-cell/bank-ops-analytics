73892dcd2931c03fc8f7a77b2a1a77c797769d3c
saherrera456-cell <saherrera456@gmail.com>
2025-11-10 14:07:46 -0500

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,6 @@
+-- Base simple
+CREATE TABLE IF NOT EXISTS transactions AS
+SELECT * FROM read_parquet('data/processed/transactions_clean.parquet');
+
+-- Índices lógicos (DuckDB usa optimizador columnar)
 
